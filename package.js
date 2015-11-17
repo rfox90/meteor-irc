2f9c1e4ed3c03feede879be52078132f3725a8b8
Richard Fox <richard.fox@ahref.co.uk>
2015-11-17 03:17:11 +0000

    Intial

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+	summary: 'A wrapper for node-irc, thats open source and up to date',
+	version: "1.0.0",
+	git: "https://github.com/rfox90/meteor-irc",
+	name: "ahref:irc"
+});
+
+Package.on_use(function (api) {
+	api.addFiles('index.js','server');
+	api.export('irc','server');
+});
+
+Npm.depends({
+	irc:'0.4.0'
+});
